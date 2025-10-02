fd8ad437d57a7415c621a572571887b3455bf04e
GuaiJieYo <guaijieaz@gmail.com>
2025-10-02 22:12:14 +0800

    refactor: 重构博客结构并添加样式优化

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
-import vue from "@astrojs/vue";
-
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.guaijie.cn",
   trailingSlash: "ignore",
-  integrations: [mdx(), sitemap(), vue()],
+  integrations: [mdx(), sitemap()],
+  build: {
+    assets: "_Meowww"
+  }
 });
